3400d77b17137651b46ec0783cbe73f51cad6ff5
mm232323 <130875143+mm232323@users.noreply.github.com>
2024-12-28 06:26:50 +0200

    direct connection setting

--- util/db/atlas_uri.js
+++ util/db/atlas_uri.js
@@ -1,2 +1,2 @@
 module.exports = uri =
-  "mongodb+srv://Mohammed_Qurany:123qweasdzxc@cluster0.4rofnps.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://Mohammed_Qurany:123qweasdzxc@cluster0.4rofnps.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?directConnection=true";
